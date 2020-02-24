a98fac0df59a8176d431f8366f559fb75c8d7227
Rudolf <kovalevskiy2017@outlook.com>
2020-02-25 01:05:27 +0300

    FunctionUpdate

--- function.js
+++ function.js
@@ -3,5 +3,21 @@
 //function calc(a, b) {
 //   return a + b;
 //}
+// let calc = function(a,b){
+//return (a + b);
+//}
 let calc = (a, b) => a + b;
 console.log(calc(5, 5));
+
+let str = "text";
+
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let twelfe = "12.2px";
+console.log(parseInt(twelfe));
+console.log(parseFloat(twelfe));
+
+let one = "33.456";
+console.log(Math.round(one));
