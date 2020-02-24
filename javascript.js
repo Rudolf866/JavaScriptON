2e7e2786d9bd73fd21916472232517e586191280
Rudolf <kovalevskiy2017@outlook.com>
2020-02-24 20:04:50 +0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,13 @@
+'use strict';
+//let person = {
+//    name: "Rudik",
+//  age: 33,
+//surname: "Kovalevskiy",
+//isCheck: false
+//};
+//alert(person.surname + " Возраст : " + person.age);
+
+//let arr = ['String', 33, "Rudik", '1235456'];
+//console.log(arr[0]);
+let answer = prompt("Вы готовы ответить :  ", "Да");
+console.log(answer);
